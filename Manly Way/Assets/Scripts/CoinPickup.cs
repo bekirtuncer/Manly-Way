4633a9bf72c4745cb673d7a84b3a86a5aab63bc7
coqorekh <bekirtuncer97@gmail.com>
2022-03-07 05:41:11 +0300

    -add persistent sfx for coins

--- Manly Way/Assets/Scripts/CoinPickup.cs
+++ Manly Way/Assets/Scripts/CoinPickup.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class CoinPickup : MonoBehaviour
 {
+    // Cached references
+    [SerializeField] AudioClip coinPickUpSFX;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        AudioSource.PlayClipAtPoint(coinPickUpSFX, Camera.main.transform.position);
         Destroy(gameObject);
     }
 }
